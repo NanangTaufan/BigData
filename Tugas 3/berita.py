4768958cd003832aed0f5cff92dd6dd8dbc807d8
Luwandino Wismar <luwandino@hotmail.com>
2016-05-27 13:11:01 +0700

    Update

--- Tugas 3/berita.py
+++ Tugas 3/berita.py
@@ -1,42 +1,41 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.mllib.feature import HashingTF
 from pyspark.mllib.feature import IDF
+import sys
 
 # Function for printing each element in RDD
 def println(x):
     print x
 
 # Boilerplate Spark stuff:
-conf = SparkConf().setMaster("local[*]").setAppName("SparkTFIDF")
+conf = SparkConf().setMaster("local").setAppName("SparkTFIDF")
 sc = SparkContext(conf = conf)
 
 # Load documents (one per line).
-rawData = sc.textFile("G:/ITS/Semester 6/Big Data/daftar-berita.csv")
-fields = rawData.map(lambda x: x.split(";"))
-documents = fields.map(lambda x: x[4].split(" "))
+rawData = sc.textFile("E:/BigData/daftar-berita.tsv")
+fields = rawData.map(lambda x: x.split("\t"))
+documents = fields.map(lambda x: x[3].lower().split(" "))
 
 documentId = fields.map(lambda x: x[0])
 
-# Creating Hash table and TF table
-hashingTF = HashingTF(100000)
+
+hashingTF = HashingTF(100000)  #100K hash buckets just to save some memory
 tf = hashingTF.transform(documents)
 
-# Creating idf
 tf.cache()
-idf = IDF(minDocFreq=1).fit(tf)
-
-# Calculate TF/IDF
+idf = IDF(minDocFreq=2).fit(tf)
 tfidf = idf.transform(tf)
 
-# Keyword yang akan dicari diubah ke Hash value <- Hash table di atas
-
-keywordTF = hashingTF.transform(["Persib"])
+keyword = str(sys.argv[1])
+keywordTF = hashingTF.transform([keyword.lower()])
+#keywordTF = hashingTF.transform(["anarchism"])
 keywordHashValue = int(keywordTF.indices[0])
 
-# Temukan relevansinya dengan tabel tf-idf yang sudah dibuat
 keywordRelevance = tfidf.map(lambda x: x[keywordHashValue])
 
 zippedResults = keywordRelevance.zip(documentId)
+tupleResult = tuple(zippedResults.max())
+idResult = str(tupleResult[1])
 
-print "Best document for keywords is:"
-print zippedResults.max()
+print "\nBest document for keyword '" + keyword + "' is id = " + idResult
+#print zippedResults.max()
--- Tugas 4/kmeans.py
+++ Tugas 4/kmeans.py
@@ -14,17 +14,15 @@ def parseLine(line):
     Lng = float(fields[14])
     return (Lt, Lng)
 
-
-    
 # Load and parse the data
 data = sc.textFile("E:/BigData/Dataset-lama.csv")
-rdd = data.map(parseLine)
-parsedData = rdd
+parsedData = data.map(parseLine)
 
-#NumCluster = str(sys.argv[1])
+
+NumCluster = int(sys.argv[1])
 
 # Build the model (cluster the data)
-clusters = KMeans.train(parsedData, 3, maxIterations=10, runs=10, initializationMode="random")
+clusters = KMeans.train(parsedData, NumCluster, maxIterations=10, runs=10, initializationMode="random")
 
 # Evaluate clustering by computing Within Set Sum of Squared Errors
 def error(point):
@@ -34,9 +32,3 @@ def error(point):
     return sqrt(sum([x**2 for x in (point - center)]))
 
 WSSSE = parsedData.map(lambda point: error(point)).reduce(lambda x, y: x + y)
-
-# print("Within Set Sum of Squared Error = " + str(WSSSE))
-
-# Save and load model
-# clusters.save(sc, "myModelPath")
-# sameModel = KMeansModel.load(sc, "myModelPath")
